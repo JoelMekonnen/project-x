aa7843f95b34392883fa7f53ac5aa4dc09104aa0
JoelMekonnen <42296192+JoelMekonnen@users.noreply.github.com>
2018-10-24 09:48:26 +0300

    Login 1.0.0
    a login panel that is nice has been added and now we work with login.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width">
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/main.css">
-        
+
     </head>
     <body>
         <div class = "whiteBox">
@@ -25,7 +25,7 @@
                          <?php  checkHome(); ?>
                     </div>
                 </div>
-                
+
                 </div>
                 <div class = "nav">
                   <ul>
@@ -59,8 +59,8 @@
                echo '<h1> about </h1>';
            }
        }
-       
-       
+
+
    }
    function createForm($type)
    {
@@ -73,49 +73,82 @@
              <div class = "LoginBox">
                 <div class = "justBorder">
                      <h3>Login</h3>
-                 <form method = "post" action = "login.php">
-                  <input type ="text" placeholder = "username" id = "txt_username"/><br>
-                     <input type = "password" placeholder = "password" id = "txt_password"/><br>
-                <input type = "submit" id = "submit" value = "Login"/><br>
+                 <form method = "post" action = "login.php" name = 'loginForm'>
+              <div id = "username_box">  <img src = "./Icons/user.png" width=30 height=40>  <input type ="text" placeholder = "username" name = "txtUser" id = "txt_username"/></div><div id = "check_Status"></div><br>
+                    <div id = "password_box">  <img src = "./Icons/padlock.png" width = 40 height = 30> <input type = "password" placeholder = "password" name = "txtUser" id = "txt_password"/><br></div>
+             <input type = "image" id = "submit" src = "./Icons/next-page.png" width = 40  height = 40 value = "Login"/><br>
                      </form>
                 </div>
             </div>
           </body>
           <style>
-             
-              #txt_username, #txt_password {
-                  border-style: solid;
-                  color: black;
+                #txt_username, #txt_password  {
+                  border-top-style: none;
+                  border-left-style: none;
+                  border-right-style: none;
+                  border-bottom-style: solid;
+                  color: white;
                   background-color:transparent;
-                  border-color: black;
+                  border-color: white;
                   border-width: 0.5;
-                  margin-left: 140px;
-                  margin-top: 18px;
-                  font-size: 15px;
+                /*  margin-let:420px;
+                  margin-top: 18px;*/
+                  font-size: 20px;
                   width: 227px;
                   padding-left: 12px;
-                  height: 30px;
-                  border-radius: 12px;
+                  height: 32px;
+                  /*border-radius: 12px;*/
+              }
+              #username_box img, #password_box img{
+                width: 40px;
+                height: 40px;
+                position: relative;
+                top: 7px;
+              }
+              #username_box, #password_box {
+                margin-left: 392px;
               }
 
               .LoginBox{
-                  margin-left: 28%;
-                  background-color: rgba(221, 221, 221, 0.5);
-                  width: 550px;
-                  height: 300px;
-                  border-radius: 30px;
+                /*  margin-left: 28%;*/
+                  background-color: rgba(128, 140, 150, 0.8);
+                  width: 100%;
+                  height: 388px;
+                  border-top-left-radius:30px;
+                  border-bottom-left-radius: 30px;
               }
               .LoginBox h3 {
                   margin: 0px;
                   padding-top: 100px;
-                  padding-left: 200px;
+                  padding-left: 470px;
+                  color: white;
                  font-family: "source sans";
-                 font-size: 30px;
+                 font-size: 41px;
               }
+              #submit{
+                margin-left: 500px;
+                margin-top: 30px;
+              }
+
           </style>
+          <script>
+
+              var myForms = document.forms.loginForm;
+              myForms.onsubmit = function(){
+                    if(myForms.txtUser.value == "")
+                    {
+                       document.getElementById("check_Status").innerHtml = "<h4>" + "please insert your username" + "</h4>";
+                       return false;
+                    }
+                    else {
+                      document.getElementById("check_Status").innerHtml = '';
+                      return true;
+                    }
+              };
+          </script>
       </html>
     <?php
       }
    }
-  
+
 ?>
